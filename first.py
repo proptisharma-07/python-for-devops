cfba21ceb7b31ac32db3aec12f26b5cd482914ac
proptisharma-07 <proptisharma@gmail.com>
2026-05-01 16:22:21 +0530

    first.py

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
-
-
-
+success starts with every challenge, not from the comfort zone.
+success is never ending failure is never final. 
+mental growth is a never-ending journey embrace it fully.
+never let success get to your head, and never let failure get to your heart.
