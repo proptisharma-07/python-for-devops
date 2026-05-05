6d4e94974a3b5f891db85b4a444f8495790802a5
proptisharma-07 <proptisharma@gmail.com>
2026-05-06 04:00:53 +0530

    6-may.py

---
+++ 5-may.py
@@ -0,0 +1,45 @@
+name = "Rahul"
+age = 26 
+#print(age >= 26)
+#print(name =="Aman")
+
+
+marks = 75
+
+#print(marks > 50)
+#print(marks < 34)
+
+city = "gwalior"
+#print(city == "gwalior")
+#print(city == "mumbai")
+
+
+price = 650
+#print(price > 650)
+#print(price <= 650)
+
+
+password = 9876
+
+#print(password == 9876)
+#print(password >= 9876)
+#print(password != 9876)
+
+age = 16
+#print(age >= 18)
+#print(age <= 16)
+
+city = "agra"
+#print(city == "Delhi")
+#print(city == "agra")
+
+
+marks = 30
+#
+#print(marks > 33 ) 
+#print(marks < 30)
+
+
+price = 2000
+print(price < 1500)
+
---
+++ 6- may.py
@@ -0,0 +1,35 @@
+age = 21
+#print(age < 18)
+
+marks = 40
+#print(marks < 40)
+#print(marks > 40)
+#print(marks >=33)
+
+
+name = "Anuj"
+#print(name == "Rahul")
+#print(name == "Aman")
+
+
+price = 1500
+#print(price > 1000)
+#print(price < 1000)
+
+
+marks = 32
+#print(price >= 33)
+#print(price <= 33)
+
+
+price = 800
+#print(price <= 500)
+#print(price >= 500)
+
+
+age = "mumbai"
+print(name == "Mumbai")
+
+      
+
+
