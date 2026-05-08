9845f3e78631cb51f8d465ae8f29487a24ea0997
proptisharma-07 <proptisharma@gmail.com>
2026-05-08 14:07:28 +0530

    8.may.py

---
+++ 8.may.py
@@ -0,0 +1,26 @@
+#1. wap to take start point and end_point from user input and print all number divsible by 2 and 3.
+
+# start_point=int(input("Enter the start point - "))
+# end_point=int(input("Enter the End point - "))
+# for i in range(start_point, end_point):
+    # if i%2==0 and i%3==0:
+    #     print(i, end=" ")
+
+#2. wap to take number from user input and print formatted table.
+# format :
+# 3*1=3
+# 3*2=6
+
+
+# num1=int(input("Enter the number - "))
+# for i in range(10,20):
+    # result=num1*i
+    # print(f"{num1} x {i} = {result}")
+
+
+#3. Wap to take a number from user input and print reversed formatted table.
+
+num1=int(input("Enter the number - "))
+for i in range(20,10,-2):
+    result=20*2
+    print(f"(20) x {i} = {result}")
