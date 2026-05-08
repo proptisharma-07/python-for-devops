4c5fd7d1bbfbbbba12d27973f85331303926695b
proptisharma-07 <proptisharma@gmail.com>
2026-05-08 10:33:27 +0530

    6-may.py

--- 6- may.py
+++ 6- may.py
@@ -48,8 +48,9 @@ res = max(num1 , num2 , num3)
 if num1>=num2 and num1>=num3:
     print(f"{num1}is greatest")
 
-if num2>=num1 and num2>=num3
-print(f"{num2}is greatest")
+if num2>=num1 and num2>=num3:
+    print(f"{num2}is greatest")
       
-if num3>=num2 and num3>=num1
-print(f"{num3}is greatest")
+if num3>=num2 and num3>=num1:
+    print(f"{num3}is greatest")
+  
