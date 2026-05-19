ddfbc2db1e7167d26be761b45fe92bf606b10a96
proptisharma-07 <proptisharma@gmail.com>
2026-05-19 14:09:57 +0530

    .\11-may.py

--- 11-may.py
+++ 11-may.py
@@ -77,3 +77,223 @@
 #     print("No profit No Loss")
 
 
+# # 19.Calculate Class Attendance Percentage
+
+# a=int(input("Percentage of class attended by student :"))
+
+# if a<75:
+#     print("Student is not allowed to sit in examination")
+# else:
+#     if a>=75:
+#        print("student is allowed to sit in examination")
+
+
+# 2. Task: Cricket Stats Analyzer 
+# ● Objective: Write a script to analyze cricket stats for a team. 
+# ● Hints: 
+# ○ Prompt the user to input the runs scored by each of the five players in a cricket match. 
+# ○ For each player (Player 1 to Player 5) ask the user to input the runs they scored. 
+# ○ Calculate the total runs scored by all players and the average runs. 
+# ○ Display the total runs and average runs to the user. 
+
+# player1 = int(input("Player1 Run Score -"))
+# player2 = int(input("Player2 Run Score -"))
+# player3 = int(input("Player3 Run Score -"))
+# player4 = int(input("Player4 Run Score -"))
+# player5 = int(input("Player5 Run Score -"))
+
+# total_runs = player1 + player2 + player3 + player4 + player5
+# avg_score = player1 + player2 + player3 + player4 + player5 / 5
+# print(f"Total Runs - {total_runs}")
+# print(f"Average Score - {avg_score}")
+
+# 17.Find the greatest number.
+
+# 17.Find the greatest number.
+
+# a=int(input("Enter your first number :"))
+# b=int(input("Enter your second number :"))
+# c=int(input("Enter your third number :"))
+
+# if a>=b and a>=c:
+#     print("The greatest number is",a)
+# if b>=a and b>=c:
+#     print("The greatest number is ",b)
+# if c>=a and c>=b:
+#     print("The greatest number is",c)        
+
+# print("congratulation your greatest number is found")
+
+
+# 16.Finding the Middle Number 
+
+# a=int(input("Enter your first number :"))
+# b=int(input("Enter your second number :"))
+# c=int(input("Enter your Third number :"))
+      
+# print(f"The three numbers are {a} {b} {c}")
+
+# if (a>=b and a<=c) or (a<=b and a>=c )  :
+#     print("Middle number is",a)
+# elif  (b>=a and b<=c) or (b<=a and b>=c) :
+#     print("Middle number is",b)
+# else:
+#     print("Middle number is ",c)
+ 
+
+# print("Congratulations You have found your middle number")  
+
+# 13. Library Charge Calculation 
+
+# a=int(input(" Enter the number of days the book has been borrowed :"))
+
+# if a<=5:
+#     print("The charges on the book is 2rupees per day")
+# elif 6<=a<=10:
+#     print("The charges on the book is 3rupees per day")  
+# elif 11<=a<=15:
+#     print("The charges on the book is 4rupees per day")      
+# elif a>=15:
+#     print("The charges on the book is 5rupees per day")    
+
+# print("-------") 
+
+# 22. Menu-Driven Login System 
+
+# a=int(input("Ask the user to Enter their Phone number :"))
+# b=int(input("Ask the user to Enter their OTP :"))
+
+# if a==1234567890 and b==1234:
+#     print("Login Successful with Phone ")
+# else:    
+#     print("OOPS, Invalid credentials")
+
+# c=(input("Enter your Email :"))
+# d=(input("Enter your Email password :"))
+
+# if c=="user@example.com" and d=="password123":
+#     print("Login Successful with Email ")
+
+# else:    
+#     print("OOPS, Invalid credentials")
+
+# user=input("exit the program :")
+# if user=="yes":
+#       print("Existing the Program,Have a nice day") 
+
+# 3. Task: Retirement Age Calculator 
+# ● Objective: Write a program that prompts the user for their age and tells them how many years until they reach retirement age (65). 
+# ● Hints: 
+# ○ Ask the user to input their age. 
+# ○ Calculate how many more years they have until they reach 65 years of age. 
+# ○ Display the number of years left until retirement or a message if the user has already reached retirement age.
+
+# age = int(input("Enter Your Age - "))
+# if age < 65:
+#     year_left = 65 - age
+#     print(f" you have {year_left} years left until Retirement")
+# else:
+#     print("You has already reached retirement age"
+
+# 5. Task: Salary Calculation 
+# ● Objective: You have to calculate an employee's salary by computing the gross salary tax and net salary based on the given parameters. 
+# ● Hints: 
+# ○ Base Salary = ₹50000 
+# ○ Bonus = ₹5000 
+# ○ Tax Rate = 10%  
+# ○ Other Charges = ₹2000 Display the Gross Salary Tax and Net Salary. 
+#base_salary = 50000
+# sal=int(input("Enter your salary: "))
+# bonus=int(input("Enter your bonus: "))
+# other_ch=int(input("Enter other charges: "))
+# tax_r=10
+# gross_sal=sal+bonus-other_ch
+# income_tax=(gross_sal*tax_r)/100
+# net_sal=gross_sal-income_tax
+# print(f"""Gross Salary Tax = {income_tax}
+# Net Salary = {net_sal}""")
+
+
+# 15. Tax Calculation for Car Purchase 
+
+# 15. Tax Calculation for Car Purchase 
+
+# a=input("Enter the Brand of the Car :") 
+# b=int(input("Enter the price of the car in lakhs :"))
+
+# if b>=7 and b<10:
+#     tax_percentage=5
+#     tax=(b*tax_percentage)/100
+#     print(f"The calculated tax on this {a} in lakhs is {tax}")
+
+# if b>=10 and b<15:
+#     tax_percentage=10
+#     tax=(b*tax_percentage)/100
+#     print(f"The calculated tax on this {a} in lakhs is {tax} ")
+
+# if  b>=15 and b<20:
+#     tax_percentage=25
+#     tax=(b*tax_percentage)/100
+#     print(f"The calculated tax on this {a} in lakhs is {tax}")
+
+# if b>=20 and b<25:
+#     tax_percentage= 30
+#     tax=(b*tax_percentage)/100
+#     print(f"The calculated tax on this {a} in lakhs is {tax}")   
+
+# print("Congratulations for your new car")    
+
+# print("🚗")
+
+
+# # 7. Task: Students Interview Eligibility Checker 
+
+# a=int(input("Enter The Academic score of a Student :"))
+# b=int(input("Enter The Attendence percentage of a Student :"))
+# c=input("Enter whether the student has participated in Extra Curriculam activities :")
+# if a>=60 and b>=75:
+#     print("cleared academic criterion")
+#     if c=="yes":
+#         print("You are Eligible for Interview")
+# else:
+#     print("You are not Eligible for Interview") 
+
+
+# radius =int(input("Enter Radius of the Circle - "))
+# area_circle = 3.14 * radius**2
+# print(f" Area of Circle - {area_circle}")
+
+
+# a=int(input("Enter your age in years :"))
+# if a>=21 and a<=32:
+#     print("you are eligible for upsc examination")
+#     b=int(input("Enter your prelims score :"))
+#     if b>=150:
+#         print("You are eligible for mains exam :")
+
+#         c=int(input("Enter your mains score :"))
+#         if c>=1000:
+#             print("You are eligible for Interview")
+
+#             d=int(input("Enter your interview marks :"))
+#             if d>=300:
+#                 print("Congratulations, You have cleared the UPSC examination")
+        
+#             else:
+#                 print("Sorry, you just missed the chance")
+
+#         else:
+#             print("You failed in your mains examination")    
+#     else:
+#         print("Sorry, you are failed in prelims")
+# else:
+#     print("You are not eligible for upsc exam")
+
+
+
+# a=input("Enter Your Email :")
+# find="gmail.com"
+# if (find in a) :
+#     print("Email is eligible for registration based on domain ")
+# else:
+#     print("Email is not eligible for Registration ")
--- 19-may.py
+++ 19-may.py
@@ -2,12 +2,46 @@
 # 1. every function has their own purpose.
 # 2. function is block of instruction(code) which execute inside its own block.
 # 3 . function is reusable means define one time use manytime(dry).
-# 4 . function has two main part first functions defination second function calling.
+# 4 . function has two main part first functions defination second function calling.\
+# 5 . in python by default none.
 
 # how define function in python.
-def add():
-    add()
-    a = 20 
-    b=10 
-    c=a+b
-    print(c)
+# def add():
+#     add()
+#     a = 20 
+#     b=10 
+#     c=a+b
+#     print(c)
+
+# function divide into 4 category.
+# 1. take nothing return nothing.
+# 2. take nothing return something.
+# 3. take something return nothing.
+# 4. take something return something.
+
+# parameters(para) and arguments(args).
+# posinal parameter/argument.
+# default parameter.
+
+# def add(a=0,b=0):
+#     print(a+b)
+# add(11,22)
+
+#def table_print(n):
+#   for i in range (1,11):
+#       print(f"{n} x {i} = {n*i}")
+#m=2
+#table_print(m)
+
+# def sub(a,b):
+#     c=a-b
+#     print(c)
+# sub(50,20)
+
+def add(a=0,b=0)
+    print("addotion :",a+b)
+
+def sub(a=0,b=0) :
+    print("subtraction :",a-b)
+
+def mul(a=0,)       
