3ea73d6cda4b2ec9f1f53aafb2fce9565ba6c17c
proptisharma-07 <proptisharma@gmail.com>
2026-05-22 15:09:51 +0530

    .\22-may.py

--- 22-may.py
+++ 22-may.py
@@ -33,3 +33,82 @@
 #     return c
 # res= index_sum("python")
 # print (res) 
+
+
+
+
+#input: base = 10, height = 6
+# output: area=?
+
+# base = 10
+# height = 6
+# area = (1/2) * base * height
+# print("Area =", area)
+
+
+#input: side = 9
+#output: perimeter =?
+
+# side = 9
+# perimeter = 6 * side 
+# print("perimeter" ,perimeter)
+
+#input: radius = 14
+# output: diameter = ?
+
+# radius = 14
+# diameter = 6*radius
+# print("Diameter", diameter)
+
+
+# side = 5
+# volume = side **2
+# print("Volume =",volume)
+
+
+# l=4
+# b=3
+# h=2
+# surface_area=2*(l*b + b*h +h*l)
+# print(surface_area)
+
+
+#input
+# x=3
+# result =x**2 - 4*x + 4
+# print(result )
+
+
+# #sum
+# x=5
+# y=7
+# result=(x+y)**2
+# print(result) 
+
+
+# #input
+# a=6
+# b=2
+# result=(a+b) * (a-b)
+# print(result)
+
+
+# #input
+# a=1
+# b=2
+# result= a**3 + b**3
+# print (result)
+
+
+# #input
+# x=10
+# y=6
+# result=x**2 - y**2
+# print(result)
+
+
+# #input 
+# x=4
+# y=1
+# result=x**3 - y**3
+# print(result)
